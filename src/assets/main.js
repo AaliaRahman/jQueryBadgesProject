4da1502f4993b5f704203158fb30c73e2fd4c26e
AaliaRahman <aaliarahma@gmail.com>
2017-06-02 03:23:40 -0500

    Almost done with project, but there is a syntax error.

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,6 +3,8 @@ $.ajax('https://www.codeschool.com/users/AaliaRahman.json', dataType:'json' {
   success: function(response){
     $('courses.completed').append('<div></div>').addClass('course');
     $('.course').append('<h3></h3>')
+    $('.course').append("<img").attr('src': 'badge');
+    $('.course').append('<a>See Course</a>').attr('href': 'url', 'target':'_blank').addClass('btn' 'btn-primary');
 }
   });
 
