f37e45e50373374fc4b4998f3eed403b98adfc46
AaliaRahman <aaliarahma@gmail.com>
2017-06-02 03:13:49 -0500

    Attempted to make changes to number 7

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,10 @@
 $(function() {
+$.ajax('https://www.codeschool.com/users/AaliaRahman.json', dataType:'json' {
+  success: function(response){
+    $('courses.completed').append('<div></div>').addClass('course');
+    $('.course').append('<h3></h3>')
+}
+  });
 
   // your code will go here
-
 });
